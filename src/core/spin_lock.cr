4fcae320d17121e51227a2ecb6463ea0d9a0b5db
Julien Portalier <julien@portalier.com>
2024-01-02 15:54:48 +0100

    SpinLock: rework using Go's fair algorithm
    The previous implementation was spinning a 100 times on an atomic swap
    before yielding to another thread, which was slower than the private
    `Crystal::SpinLock`.
    
    This new version implement's Go's futex algorithm, where we spin 4 times
    trying to swap the atomic while easing the cpu between each attempts (to
    avoid burning) before yielding to another thread.
    
    The futex sleep/wakeup wasn't ported as the CAS operations (to keep the
    SLEEPING state) and the actual futex were slower in both a naive
    benchmark, where all threads are trying to lock, and another benchmark
    where only half the threads are trying to lock while the other half are
    busy doing the same CPU bound operation but without the lock.
    
    The new algorithm proved faster on high contention, and perfectly stable
    once we have more threads than available CPU cores, which the exception
    of MT:8 for which `Crystal::SpinLock` performed better on the all
    threads fight for the lock benchmark (but not the distributed
    lock/nolock one).

--- src/core/spin_lock.cr
+++ src/core/spin_lock.cr
@@ -1,58 +1,99 @@
-require "./atomic_lock"
+require "./lockable"
 
 module Syn::Core
-  # Tries to acquire an atomic lock by spining, trying to avoid slow thread
-  # context switches that involve the kernel scheduler but eventually fallback
-  # to yielding to another thread, to avoid spinning (and thus burning) a CPU
-  # that would be counter-productive (scheduler may believe the thread is doing
-  # some heavy computation and delay it's suspension).
-  #
   # This is a public alternative to the private Crystal::SpinLock in stdlib.
-  # That beind said, you're not supposed to ever need it.
+  # You're not supposed to ever need it. The implementation is a NOOP until MT
+  # was enabled at compile time (i.e. `-Dpreview_mt` flag).
   #
-  # The implementation is a NOOP until the program has enabling MT during
-  # compilation (i.e. `-Dpreview_mt` flag).
+  # Warning:
+  # - failing to acquire the lock will block the current thread!
+  # - acquiring the lock will block other threads waiting on the lock!
+  #
+  # The mutually exclusive running sections must be as small and fast as
+  # possible, otherwise the program performance will be impacted!
   struct SpinLock
+    include Lockable
+
     {% if flag?(:preview_mt) %}
-      # :nodoc:
-      THRESHOLD = 100
+      UNLOCKED = 0_i8
+      LOCKED = 1_i8
 
-      @lock = AtomicLock.new
+      @m = Atomic(Int8).new(UNLOCKED)
 
+      @[AlwaysInline]
       def lock : Nil
-        until @lock.acquire?
-          # fixed busy loop to avoid a context switch:
-          count = THRESHOLD
-          until (count -= 1) == 0
-            return if @lock.acquire?
-          end
-
-          # fallback to thread context switch (slow path):
-          Thread.yield
+        if @m.swap(LOCKED) == UNLOCKED
+          {% if flag?(:arm) %} Syn.fence(:acquire) {% end %}
+        else
+          lock_slow
         end
       end
 
+      # This is based on Go's futex loop (but without the futex) which is
+      # distributed under a BSD-like license. See:
+      #
+      # https://cs.opensource.google/go/go/+/refs/tags/go1.21.5:src/runtime/lock_futex.go;l=51
+      # https://cs.opensource.google/go/go/+/refs/tags/go1.21.5:LICENSE
+      @[NoInline]
+      private def lock_slow : Nil
+        # OPTIMIZE: only spin if NCPU > 1 && NPROCS > 1
+        active_spin = 4
+        passive_spin = 1
+
+        loop do
+          # try to lock, spinning
+          active_spin.times do
+            while @m.lazy_get == UNLOCKED
+              if @m.swap(LOCKED) == UNLOCKED
+                {% if flag?(:arm) %} Syn.fence(:acquire) {% end %}
+                return
+              end
+            end
+
+            # yield cpu (low power) to avoid burning the CPU and tricking
+            # the OS scheduler from believing the current thread needs power
+            30.times { Intrinsics.pause }
+          end
+
+          # try to lock, rescheduling
+          passive_spin.times do
+            while @m.lazy_get == UNLOCKED
+              if @m.swap(LOCKED) == UNLOCKED
+                {% if flag?(:arm) %} Syn.fence(:acquire) {% end %}
+                return
+              end
+            end
+            Thread.yield
+          end
+        end
+      end
+
+      @[AlwaysInline]
       def unlock : Nil
-        @lock.release
+        @m.set(UNLOCKED)
+        {% if flag?(:arm) %} Syn.fence(:release) {% end %}
+      end
+
+      @[AlwaysInline]
+      def synchronize(& : -> U) : U forall U
+        lock
+        yield
+      ensure
+        unlock
       end
     {% else %}
+      @[AlwaysInline]
       def lock : Nil
       end
 
+      @[AlwaysInline]
       def unlock : Nil
       end
-    {% end %}
 
-    # Locks, yields and unlocks.
-    #
-    # NOTE: make sure that the block doesn't execute anything more than
-    # necessary, since concurrent threads will be blocked for its whole
-    # duration!
-    def synchronize(& : -> U) : U forall U
-      lock
-      yield
-    ensure
-      unlock
-    end
+      @[AlwaysInline]
+      def synchronize(& : -> U) : U forall U
+        yield
+      end
+    {% end %}
   end
 end
